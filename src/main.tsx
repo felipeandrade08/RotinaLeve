4348c94193a18c581fdec1cf01efa62b3722259a
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-08-26 16:22:39 -0300

    fix(brand): load visual identity through Vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import "./index.css";
+import "./brand.css";
 import InstallBanner from "./components/InstallBanner";
 import {TaskProvider} from "./context/TaskContext";
 import {FinanceProvider} from "./context/FinanceContext";
