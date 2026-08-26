8a74b02f4045e555bde6e1b7048b49db1c2cffe4
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-08-25 23:57:53 -0300

    feat: register service worker for PWA

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
-
 import { TaskProvider } from "./context/TaskContext";
 import { FinanceProvider } from "./context/FinanceContext";
 import { EventProvider } from "./context/EventContext";
 import { ReminderProvider } from "./context/ReminderContext";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {
+      // O app continua funcionando normalmente sem o service worker.
+    });
+  });
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
